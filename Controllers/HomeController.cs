91e7bf8327971f638e3b6b34d4c31704de35d781
taieb <belaid.taieb@gmail.com>
2023-09-07 12:55:03 +0200

    getting information from form

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet("result")]
+    public ViewResult Result(User newUser){
+        ViewBag.User = newUser;
+        return View("Result");
+    }
+
+    [HttpPost("process")]
+    public IActionResult Process(User newUser) {
+        return RedirectToAction("Result",newUser);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace DojoSurvey.Models;
+#pragma warning disable CS8618
+
+public class User
+{
+    public string Name {get;set;}
+    public string Location {get;set;} 
+    public string Language {get;set;}
+    public string Comment {get;set;}
+}
